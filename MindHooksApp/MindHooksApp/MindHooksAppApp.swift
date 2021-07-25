30e32bdf1a24813588a0936290ca629ca1b2e162
Alexandr <alexandrlobanov@outlook.com>
2021-07-25 11:48:02 +0300

    Project setup + settings

---
+++ MindHooksApp/MindHooksApp/MindHooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindHooksAppApp.swift
+//  MindHooksApp
+//
+//  Created by Alexandr L. on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MindHooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
