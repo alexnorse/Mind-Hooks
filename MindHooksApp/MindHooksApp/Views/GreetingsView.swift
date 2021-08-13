45ac9b8501bc21d5a9ba2cff33becfd70871a167
Alexandr <alexandrlobanov@outlook.com>
2021-08-13 15:17:55 +0300

    GreetingsView issue

--- MindHooksApp/MindHooksApp/Views/GreetingsView.swift
+++ MindHooksApp/MindHooksApp/Views/GreetingsView.swift
@@ -12,48 +12,29 @@ struct GreetingsView: View {
     @State private var isVisible = false
     
     var body: some View {
-        NavigationView {
-            VStack(alignment: .center) {
-                ZStack {
-                    Circle()
-                        .foregroundColor(.gray)
-                        .opacity(0.1)
-                        .blur(radius: 5)
-                        .frame(width: 350, height: 350)
-                    
-                        .opacity(isVisible ? 1 : 0)
-                        .offset(y: isVisible ? 0 : 20)
-                        .animation(Animation.easeOut(duration: 0.5).delay(0.2))
-                        .onAppear { self.isVisible.toggle() }
-                    
-                    VStack(alignment: .center, spacing: 1) {
-                        GreetingsLogo()
-                            .offset(y: UInumbers.offsetY + 120)
-                        
-                        AccentText(text: Descriptions.greetings)
-                            .offset(y: UInumbers.offsetY + 90)
-                        
-                        DescriptionText(text: Descriptions.startDay)
-                            .offset(y: UInumbers.offsetY + 95)
-                    }
-                    .offset(y: UInumbers.offsetY)
-                }
+        ZStack {
+            Circle()
+                .foregroundColor(.gray)
+                .opacity(0.1)
+                .blur(radius: 5)
+                .frame(width: 350, height: 350)
                 
-                NavigationLink(
-                    destination: MainView(), label: {
-                        DescriptionText(text: Descriptions.navlink)
-                            .frame(width: 150, height: 50, alignment: .center)
-                            .foregroundColor(.white)
-                            .background(Color.accentColor)
-                            .cornerRadius(30)
-                            .shadow(color: .gray.opacity(ShadowSettings.opacity),
-                                    radius: ShadowSettings.radius,
-                                    x: ShadowSettings.x,
-                                    y: ShadowSettings.y)
-                    })
-                    .offset(y: UInumbers.navlinkY)
+                .opacity(isVisible ? 1 : 0)
+                .offset(y: isVisible ? 0 : 20)
+                .animation(Animation.easeOut(duration: 0.5).delay(0.2))
+                .onAppear { self.isVisible.toggle() }
+            
+            VStack(alignment: .center, spacing: 1) {
+                GreetingsLogo()
+                    .offset(y: UInumbers.offsetY + 150)
+                
+                AccentText(text: Descriptions.greetings)
+                    .offset(y: UInumbers.offsetY + 120)
+                
+                DescriptionText(text: Descriptions.startDay)
+                    .offset(y: UInumbers.offsetY + 125)
             }
-            .navigationBarHidden(true)
+            .offset(y: UInumbers.offsetY - 30)
         }
     }
 }
