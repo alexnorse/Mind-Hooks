9c73d51bc240083e6405a683edf6a765ee639022
Alexandr <alexandrlobanov@outlook.com>
2021-10-01 11:43:04 +0300

    Thought of the day - rephrase

--- MindHooksApp/MindHooksApp/Helpers/Constants.swift
+++ MindHooksApp/MindHooksApp/Helpers/Constants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 enum CategoryHeads {
     static let word   = "Word of the day"
-    static let quote  = "Quote of the day"
+    static let quote  = "Thought of the day"
     static let event  = "This day in history"
 }
 
