fd93dba05a87ac5374938292b6b1563b1321a9e0
Alexandr <alexsavvin@outlook.com>
2022-07-10 13:10:07 +0300

    MH light target setup

---
+++ MindHooksApp/MindHooks Light/MindHooks_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindHooks_LightApp.swift
+//  MindHooks Light
+//
+//  Created by Alexandr on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MindHooks_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
